71ad89b35d5dee8d9b9b5d5719b63e5c35ee9f57
Alex <Alrast95@gmail.com>
2021-03-01 21:14:59 +0400

    классы бизнес модели 1.2

--- src/main/java/ru/linarkou/loans/v1/model/Client.java
+++ src/main/java/ru/linarkou/loans/v1/model/Client.java
@@ -1,7 +1,18 @@
 package ru.linarkou.loans.v1.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Client {
-    Integer id;
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
     String fio;
     Integer telNumber;
     String email;
